2685a10cfcb890ca9e9c585d2a3122c2a282946c
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-12-24 01:04:57 +0800

    add a component test

--- tests/integration/components/edit-note/component-test.js
+++ tests/integration/components/edit-note/component-test.js
@@ -1,25 +1,53 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('edit-note', 'Integration | Component | edit note', {
-  integration: true
+  needs: [
+    'component:markdown-to-html'
+  ]
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  var component = this.subject();
+  assert.expect(4);
+
+  assert.equal(component._state, 'preRender');
+
+  this.render();
+  assert.equal(component._state, 'inDOM');
 
   this.render(hbs`{{edit-note}}`);
+  assert.equal(this.$('button:first').text().trim(), 'save');
+  assert.equal(this.$('button:last').text().trim(), 'close');
+});
 
-  assert.equal(this.$('button:first').text().trim(), "save");
-  assert.equal(this.$('button:last').text().trim(), "close");
+test('it saves', function(assert) {
+  var component = this.subject();
+  this.render();
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#edit-note}}
-      template block text
-    {{/edit-note}}
-  `);
+  var saveTarget = {
+    save() {
+      assert.ok(true, 'saved the note');
+    }
+  };
+  Ember.run(()=> {
+    component.set('note', saveTarget);
+  });
+  this.$().find('#save').click();
+});
 
-  assert.equal(this.$('div:last').text().trim(), 'template block text');
+test('it close', function(assert) {
+  var component = this.subject();
+  this.render();
+
+  var closeTarget = {
+    closeAction() {
+      assert.ok(true, 'closed the window');
+    }
+  };
+
+  component.set('close', 'closeAction');
+  component.set('targetObject', closeTarget);
+  this.$().find('#close').click();
 });
