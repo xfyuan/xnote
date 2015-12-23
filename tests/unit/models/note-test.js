4bf71e0c9f756a052f8591298ef57f20d2c128af
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-12-24 00:13:40 +0800

    fix tests

--- tests/unit/models/note-test.js
+++ tests/unit/models/note-test.js
@@ -2,7 +2,9 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('note', 'Unit | Model | note', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    'model:notebook'
+  ]
 });
 
 test('it exists', function(assert) {
--- tests/unit/models/notebook-test.js
+++ tests/unit/models/notebook-test.js
@@ -2,7 +2,10 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('notebook', 'Unit | Model | notebook', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: [
+    'model:user',
+    'model:note'
+  ]
 });
 
 test('it exists', function(assert) {
