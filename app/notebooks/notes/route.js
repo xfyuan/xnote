dff832b8b6f10497f26cf4d34f03fa6ae60e1060
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-12-24 22:17:37 +0800

    add a utility

--- app/notebooks/notes/route.js
+++ app/notebooks/notes/route.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import isValidLength from 'xnote/utils/is-valid-length';
 
 export default Ember.Route.extend({
   model(params) {
@@ -7,21 +8,26 @@ export default Ember.Route.extend({
 
   actions: {
     addNote() {
-      this.store.findRecord('notebook', this.paramsFor('notebooks.notes').notebook_id).then((notebook)=> {
-        console.log(notebook);
-        var note = this.store.createRecord('note', {
-          title: this.controller.get('title'),
-          notebook: notebook
+      var title = this.controller.get('title');
+      if (!isValidLength(title, 0, 140)) {
+        alert('Title must be longer than 0 and not more than 140');
+      } else {
+        this.store.findRecord('notebook', this.paramsFor('notebooks.notes').notebook_id).then((notebook)=> {
+          console.log(notebook);
+          var note = this.store.createRecord('note', {
+            title: this.controller.get('title'),
+            notebook: notebook
+          });
+          console.log(note);
+          note.save().then(()=> {
+            console.log('save successful');
+            this.controller.set('title', null);
+            this.refresh();
+          }, ()=> {
+            console.log('save failed');
+          });
         });
-        console.log(note);
-        note.save().then(()=> {
-          console.log('save successful');
-          this.controller.set('title', null);
-          this.refresh();
-        }, ()=> {
-          console.log('save failed');
-        });
-      });
+      }
     },
     deleteNote(note) {
       console.log('deleting note with title' + note.get('title'));
